e01f3ee8ca650081b0eefdc8ea618181547f6e38
eslamhussein <eslam.hussein.awad@gmail.com>
2018-12-18 13:05:35 +0200

    Create custom Cluster to handle zoom in - out in maps

--- client/app/src/main/java/com/glovo/glovo/map/view/MapActivity.kt
+++ client/app/src/main/java/com/glovo/glovo/map/view/MapActivity.kt
@@ -5,31 +5,33 @@ import android.os.Bundle
 import android.util.Log
 import com.glovo.glovo.R
 import com.glovo.glovo.base.view.MvpActivity
+import com.glovo.glovo.ext.ConvexHull
 import com.glovo.glovo.map.data.dto.City
 import com.glovo.glovo.map.data.dto.Country
 import com.glovo.glovo.map.presenter.MapPresenter
-import com.google.android.gms.common.util.MapUtils
+import com.glovo.glovo.map.view.cluster.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolygonOptions
 import com.google.maps.android.PolyUtil
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import java.util.ArrayList
+import com.google.maps.android.clustering.ClusterManager
 
 
 private val TAG = "TAG${MapActivity::class.java.simpleName}"
 
-class MapActivity : MvpActivity<MainView, MapPresenter>(), MainView, OnMapReadyCallback {
-
+class MapActivity : MvpActivity<MainView, MapPresenter>(), MainView, OnMapReadyCallback,
+    ClusterManager.OnClusterItemClickListener<CityClusterItem> {
 
     override val presenter: MapPresenter by inject { parametersOf(this) }
 
     private lateinit var mMap: GoogleMap
+    private var mClusterItemManager: ClusterManager<CityClusterItem>? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,45 +48,75 @@ class MapActivity : MvpActivity<MainView, MapPresenter>(), MainView, OnMapReadyC
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
+        mClusterItemManager = ClusterManager(this, mMap)
+        mClusterItemManager?.algorithm = ClusterAlgorithm()
+        val renderer = CitiesClusterRenderer(this, mMap, mClusterItemManager!!)
+        mClusterItemManager?.renderer = renderer
+        mMap.setOnCameraIdleListener(mClusterItemManager)
+        mMap.setOnMarkerClickListener(mClusterItemManager)
+        mClusterItemManager?.setOnClusterItemClickListener(this)
         presenter.getCities()
-
 
     }
 
 
     override fun showLoading() {
-        Log.d(TAG, "showLoading")
+
+
 
     }
 
 
     override fun hideLoading() {
-        Log.d(TAG, "hideLoading")
+
 
     }
 
     override fun showCountries(countries: List<Country>) {
-        Log.d(TAG, "showCountries ${countries.size}")
+
 
     }
 
     override fun showError(error: String) {
-        Log.d(TAG, "showError")
+
+
+
     }
 
     override fun showCities(cities: List<City>) {
-        Log.d(TAG, "showCities ${cities.size}")
 
+        for (city in cities) {
 
+            val polygons = ArrayList<PolygonOptions>()
+            val polygonOptions = PolygonOptions()
 
+            for (polygonEncoded in city.workingArea) {
 
+                polygonOptions.addAll(PolyUtil.decode(polygonEncoded))
+                polygons.add(polygonOptions)
+            }
+
+            val polygonOption = ConvexHull.convert(polygons).strokeColor(Color.RED)?.fillColor(Color.MAGENTA)
+            val polygon = mMap.addPolygon(polygonOption)
+
+            mClusterItemManager?.addItem(
+                CityClusterItem(
+                    polygon?.getCenterPoint()!!,
+                    city.name,
+                    city.countryCode,
+                    city.countryCode, polygon.getBounds()
+                )
+            )
+        }
 
     }
+
+    override fun onClusterItemClick(pin: CityClusterItem?): Boolean {
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(pin?.bounds, 0))
+        return true
+    }
+
 
     override fun showCityDetails(city: City) {
         Log.d(TAG, "showCityDetails ${city.name}")
---
+++ client/app/src/main/java/com/glovo/glovo/map/view/cluster/CitiesClusterRenderer.kt
@@ -0,0 +1,46 @@
+package com.glovo.glovo.map.view.cluster
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.glovo.glovo.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.Cluster
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.clustering.view.DefaultClusterRenderer
+import com.google.maps.android.ui.IconGenerator
+
+
+class CitiesClusterRenderer(
+    private val context: Context, map: GoogleMap, clusterItemManager: ClusterManager<CityClusterItem>
+) : DefaultClusterRenderer<CityClusterItem>(context, map, clusterItemManager) {
+
+
+    override fun onBeforeClusterItemRendered(item: CityClusterItem?, markerOptions: MarkerOptions?) {
+        super.onBeforeClusterItemRendered(item, markerOptions)
+
+        val markerDescriptor: BitmapDescriptor =
+            BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW);
+
+        markerOptions?.icon(markerDescriptor)?.snippet(item?.title);
+
+    }
+
+    override fun onBeforeClusterRendered(cluster: Cluster<CityClusterItem>?, markerOptions: MarkerOptions?) {
+        super.onBeforeClusterRendered(cluster, markerOptions)
+
+        val mClusterIconGenerator: IconGenerator = IconGenerator(context.getApplicationContext())
+
+        mClusterIconGenerator.setBackground(
+            ContextCompat.getDrawable(context, R.drawable.background_circle)
+        );
+        mClusterIconGenerator.setTextAppearance(R.style.AppTheme_WhiteTextAppearance)
+        val icon = mClusterIconGenerator.makeIcon(cluster?.items?.first()?.snippet)
+        markerOptions?.icon(BitmapDescriptorFactory.fromBitmap(icon));
+
+
+    }
+
+}
